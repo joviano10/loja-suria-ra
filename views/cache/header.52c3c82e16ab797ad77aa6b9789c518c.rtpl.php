b128f4c4c9a14c29cc82aaa91bb56e481ebabfd9
joviano <adjovian@gmail.com>
2020-04-27 17:48:43 -0300

    Page

---
+++ views/cache/header.52c3c82e16ab797ad77aa6b9789c518c.rtpl.php
@@ -0,0 +1,10 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Loja suria-ra</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+    
